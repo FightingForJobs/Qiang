bb6ea975e11da916b0ed3f782b489e60550981d9
ZhangQiang <JackZhang@ZhangQiangs-MacBook-Pro.local>
2016-11-12 17:22:51 -0500

    efficient Ransom Note

--- Ransom Note/Solution.java
+++ Ransom Note/Solution.java
@@ -16,3 +16,21 @@ public class Solution {
         }
     
 }
+
+//public class Solution {
+//    public boolean canConstruct(String ransomNote, String magazine) {
+//        int[] alphabeta = new int[26];
+//        
+//        for(int i = 0; i < magazine.length(); i++){
+//            alphabeta[magazine.charAt(i) - 97]++;
+//        }
+//        for(int j = 0; j < ransomNote.length(); j++){
+//            alphabeta[ransomNote.charAt(j) - 97]--;
+//            if(alphabeta[ransomNote.charAt(j) - 97]<0){
+//                return false;
+//            }
+//        }   
+//        return true;
+//       
+//    }
+//}
