fee5f91321921e23025ec2d9aac0d6472bb28daa
ZhangQiang <JackZhang@ZhangQiangs-MacBook-Pro.local>
2016-11-12 01:40:59 -0500

    Find the Difference

---
+++ Find the Difference/Solution.java
@@ -0,0 +1,14 @@
+
+public class Solution {
+    public char findTheDifference(String s, String t) {
+        HashSet<Character> set = new HashSet<Character>();
+        char[] charArray = (s+t).toCharArray();
+        for(char c:charArray){
+            if(!set.add(c)){
+                set.remove(c);
+            }
+        }
+        Iterator<Character> it = set.iterator();
+        return it.next();
+    }
+}
